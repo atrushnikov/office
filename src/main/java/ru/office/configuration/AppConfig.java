7e723e8203b38b87885eff03122508d2d3e79cd1
a.trushnikov <a.trushnikov@gmail.com>
2019-05-24 13:42:57 +0300

    add congig file

---
+++ src/main/java/ru/office/configuration/AppConfig.java
@@ -0,0 +1,96 @@
+package ru.office.configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+import liquibase.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "ru.office.repository")
+@EnableTransactionManagement
+public class AppConfig {
+
+    private Environment environment;
+
+    @Value("${datasource.office.maxPoolSize}")
+    private int maxPoolSize;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.office")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceProperties properties = dataSourceProperties();
+        HikariDataSource dataSource = DataSourceBuilder
+                .create(properties.getClassLoader())
+                .driverClassName(properties.getDriverClassName())
+                .url(properties.getUrl())
+                .username(properties.getUsername())
+                .password(properties.getPassword())
+                .type(HikariDataSource.class)
+                .build();
+        dataSource.setMaximumPoolSize(maxPoolSize);
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+    private Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.office.hibernate.dialect"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("datasource.office.hibernate.hbm2ddl.method"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.office.hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.office.hibernate.format_sql"));
+        if(StringUtils.isNotEmpty(environment.getRequiredProperty("datasource.office.defaultSchema"))){
+            properties.put("hibernate.default_schema", environment.getRequiredProperty("datasource.office.defaultSchema"));
+        }
+        return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setPackagesToScan("ru.office.model");
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        factoryBean.setJpaProperties(jpaProperties());
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(emf);
+        return txManager;
+    }
+
+}
